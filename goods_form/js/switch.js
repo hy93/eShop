ab0ec8696c225f44f929971d3787a7b7bb204fc8
Han Ying <azxdhy@163.com>
2016-11-30 22:05:07 +0800

    增加店主管理页面（只完成了侧边菜单）

--- goods_form/js/switch.js
+++ goods_form/js/switch.js
@@ -1,3 +1,12 @@
+function step1(){
+	document.getElementById("step1").style.display="block";
+	document.getElementById("step2").style.display="none";	
+	document.getElementById("step3").style.display="none";	
+	document.getElementById("step4").style.display="none";	
+	document.getElementById("step5").style.display="none";	
+	document.getElementById("step6").style.display="none";	
+	document.getElementById("over").style.display="none";
+}
 function step2(){
 	document.getElementById("step1").style.display="none";
 	document.getElementById("step2").style.display="block";	
